ecebc8666eb4a62845bde525f78bbb95de1baca4
Florian <metaln00b@web.de>
2021-05-26 21:10:38 +0200

    Some code cleanup

--- ESP32-SimHubBT-4B.ino
+++ ESP32-SimHubBT-4B.ino
@@ -15,6 +15,13 @@
 #define DUTY_MAX                ((1 << LEDC_TIMER_10_BIT) -1 )
 #define FREQ_MIN_Hz             1 /* Do not decrease it! */
 
+#define FUEL_DUTY_CYCLE_MIN     100     /* FULL     */
+#define FUEL_DUTY_CYCLE_MAX     540     /* EMPTY    */
+#define TEMP_DUTY_CYCLE_MIN     26      /* 60°C     */
+#define TEMP_DUTY_CYCLE_MAX     512     /* >120°C   */
+#define OILTEMP_DUTY_CYCLE_MIN  12      /* 60°C     */
+#define OILTEMP_DUTY_CYCLE_MAX  600     /* >180°C   */
+#define OIL_WARN_TRESH_RPM      300
 
 #define TURN_R_PIN              5
 #define FUEL_PIN                2
@@ -32,25 +39,11 @@
 #define DIMM_PIN                33
 #define PWR_PIN                 17
 
-#define BUF_SIZE                128
+#define BUF_SIZE                64
 
-char simHubMessageBuf[BUF_SIZE];
-BluetoothSerial btSerial;
+char simhub_message_buf[BUF_SIZE];
+BluetoothSerial bt_serial;
 
-float revs;
-float speed_kmh;
-float fuel_percent;
-float water_temperature_degC;
-int turn_left;
-int turn_right;
-int brake;
-float oil_temperature_degC;
-
-unsigned int temp_duty_cycle = 0;
-float speed_Hz = 0;
-float rpm_Hz = 0;
-unsigned int fuel_duty_cycle = 0;
-unsigned int oil_temp_duty_cycle = 0;
 
 void ledc_init(uint8_t pin, float freq_Hz, ledc_channel_t channel, ledc_timer_t timer) {
     const char * ME = __func__;
@@ -137,23 +130,32 @@ void setup() {
 
     digitalWrite(PWR_PIN, HIGH);
 
-    memset(simHubMessageBuf, 0x0, BUF_SIZE);
-    btSerial.begin(BT_NAME);
+    memset(simhub_message_buf, 0x0, BUF_SIZE);
+    bt_serial.begin(BT_NAME);
 }
 
 void loop() {
-    if (btSerial.available() > 0)
+    if (bt_serial.available() > 0)
     {
-        btSerial.readBytesUntil('{', simHubMessageBuf, BUF_SIZE);
-        int readCount = btSerial.readBytesUntil('}', simHubMessageBuf, BUF_SIZE);
-        simHubMessageBuf[min(readCount, BUF_SIZE - 1)] = 0x0;
-        processMessage();
-        memset(simHubMessageBuf, 0x0, BUF_SIZE);
+        bt_serial.readBytesUntil('{', simhub_message_buf, BUF_SIZE);
+        int readCount = bt_serial.readBytesUntil('}', simhub_message_buf, BUF_SIZE);
+        simhub_message_buf[min(readCount, BUF_SIZE - 1)] = 0x0;
+        process_message();
+        memset(simhub_message_buf, 0x0, BUF_SIZE);
     }
 }
 
-void processMessage() {
-    sscanf(simHubMessageBuf, "%f&%f&%f&%f&%d&%d&%d&%f",
+void process_message() {
+    unsigned int revs;
+    unsigned int speed_kmh;
+    unsigned int fuel_percent;
+    float water_temperature_degC;
+    int turn_left;
+    int turn_right;
+    int brake;
+    float oil_temperature_degC;
+    
+    sscanf(simhub_message_buf, "%u&%u&%u&%f&%d&%d&%d&%f",
         &revs,
         &speed_kmh,
         &fuel_percent,
@@ -164,17 +166,75 @@ void processMessage() {
         &oil_temperature_degC
     );
 
-    if (revs >= 200)
+    float rpm_Hz = rpm_get_Hz(revs);
+    float speed_Hz = speed_get_Hz(speed_kmh);
+    unsigned int fuel_duty_cycle = fuel_duty_cycle_get(fuel_percent);
+    unsigned int temp_duty_cycle = water_temperature_duty_cycle_get(water_temperature_degC);
+    unsigned int oil_temp_duty_cycle = oil_temperature_duty_cycle_get(oil_temperature_degC);
+
+    handle_lights(turn_left, turn_right, brake, revs);
+
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, temp_duty_cycle);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+
+    ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_1, rpm_Hz);
+    ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_2, speed_Hz);
+
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3, fuel_duty_cycle);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3);
+
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4, oil_temp_duty_cycle);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4);
+}
+
+float rpm_get_Hz(float revs) {
+    float freq_Hz = 0.05 * revs;
+
+    if (freq_Hz < FREQ_MIN_Hz)
     {
-        digitalWrite(OIL_PIN, HIGH);
-    }
-    if (revs < 200)
-    {
-        digitalWrite(OIL_PIN, LOW);
+        return FREQ_MIN_Hz;
     }
     
-    rpm_Hz = 0.05 * revs;
+    return freq_Hz;
+}
 
+float speed_get_Hz(float speed_kmh) {
+    float freq_Hz = 1.087 * speed_kmh - 0.6;
+
+    if (freq_Hz < FREQ_MIN_Hz)
+    {
+        return FREQ_MIN_Hz;
+    }
+    
+    return freq_Hz;
+}
+
+unsigned int fuel_duty_cycle_get(float fuel_percent) {
+    unsigned int fuel_duty_cycle = FUEL_DUTY_CYCLE_MIN;
+    
+    if (fuel_percent > 0.0 && fuel_percent <= 25.0)
+    {
+        fuel_duty_cycle = -9.6 * fuel_percent + 540;
+    }
+    else if (fuel_percent > 25.0 && fuel_percent <= 50.0)
+    {
+        fuel_duty_cycle = -4.4 * fuel_percent + 410;
+    }
+    else if (fuel_percent > 50.0 && fuel_percent <= 75.0)
+    {
+        fuel_duty_cycle = -2.08 * fuel_percent + 294;
+    }
+    else if (fuel_percent > 75.0 && fuel_percent <= 100.0)
+    {
+        fuel_duty_cycle = -1.52 * fuel_percent + 252;
+    }
+    
+    return fuel_duty_cycle;
+}
+
+unsigned int water_temperature_duty_cycle_get(float water_temperature_degC) {
+    unsigned int temp_duty_cycle = TEMP_DUTY_CYCLE_MIN;
+    
     if (water_temperature_degC > 60.0 && water_temperature_degC <= 80.0)
     {
         temp_duty_cycle = water_temperature_degC - 34;
@@ -192,25 +252,17 @@ void processMessage() {
         temp_duty_cycle = 21.0 * water_temperature_degC - 2100;
     }
 
-    speed_Hz = 1.087 * speed_kmh - 0.6;
-
-    if (fuel_percent > 0.0 && fuel_percent <= 25.0)
+    if (temp_duty_cycle >= TEMP_DUTY_CYCLE_MAX)
     {
-        fuel_duty_cycle = -9.6 * fuel_percent + 540;
-    }
-    else if (fuel_percent > 25.0 && fuel_percent <= 50.0)
-    {
-        fuel_duty_cycle = -4.4 * fuel_percent + 410;
-    }
-    else if (fuel_percent > 50.0 && fuel_percent <= 75.0)
-    {
-        fuel_duty_cycle = -2.08 * fuel_percent + 294;
-    }
-    else if (fuel_percent > 75.0 && fuel_percent <= 100.0)
-    {
-        fuel_duty_cycle = -1.52 * fuel_percent + 252;
+        return TEMP_DUTY_CYCLE_MAX;
     }
 
+    return temp_duty_cycle;
+}
+
+unsigned int oil_temperature_duty_cycle_get(float oil_temperature_degC) {
+    unsigned int oil_temp_duty_cycle = OILTEMP_DUTY_CYCLE_MIN;
+    
     if (oil_temperature_degC > 60.0 && oil_temperature_degC <= 100.0)
     {
         oil_temp_duty_cycle = 0.9 * oil_temperature_degC - 42;
@@ -232,6 +284,30 @@ void processMessage() {
         oil_temp_duty_cycle = 17.5 * oil_temperature_degC - 2570;
     }
 
+    if (oil_temp_duty_cycle >= OILTEMP_DUTY_CYCLE_MAX)
+    {
+        return OILTEMP_DUTY_CYCLE_MAX;
+    }
+
+    return oil_temp_duty_cycle;
+}
+
+void handle_lights(
+    int turn_left,
+    int turn_right,
+    int brake,
+    unsigned int revs
+)
+{
+    if (revs >= OIL_WARN_TRESH_RPM)
+    {
+        digitalWrite(OIL_PIN, HIGH);
+    }
+    if (revs < OIL_WARN_TRESH_RPM)
+    {
+        digitalWrite(OIL_PIN, LOW);
+    }
+    
     if (turn_right != 0)
     {
         digitalWrite(TURN_R_PIN, HIGH);
@@ -255,35 +331,5 @@ void processMessage() {
     else
     {
         digitalWrite(BRAKE_PIN, LOW);
-    }
-
-    if (temp_duty_cycle >= 26 && temp_duty_cycle <= 512)
-    {
-        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, temp_duty_cycle);
-        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
-    }
-
-    if (rpm_Hz < FREQ_MIN_Hz)
-    {
-        rpm_Hz = FREQ_MIN_Hz;
-    }
-    ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_1, rpm_Hz);
-
-    if (speed_Hz < FREQ_MIN_Hz)
-    {
-        speed_Hz = FREQ_MIN_Hz;
-    }
-    ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_2, speed_Hz);
-
-    if (fuel_duty_cycle >= 100 && fuel_duty_cycle <= 540)
-    {
-        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3, fuel_duty_cycle);
-        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3);
-    }
-
-    if (oil_temp_duty_cycle >= 12 && oil_temp_duty_cycle <= 600)
-    {
-        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4, oil_temp_duty_cycle);
-        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4);
     }
 }
